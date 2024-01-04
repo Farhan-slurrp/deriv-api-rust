dfc06ae5a38337ff384ca47458bd2281914020d3
Farhan Ahmad Nurzi <farhanlmntrix@gmail.com>
2024-01-04 12:17:09 +0800

    mutation and trying arc mutex

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use futures_util::{SinkExt, StreamExt};
-use std::str;
+use serde_json;
+use std::{
+    str,
+    sync::{Arc, Mutex},
+    vec,
+};
 use tokio::{
     io::{AsyncBufReadExt, Result},
     net::TcpStream,
@@ -13,11 +18,6 @@ struct WebSocket {
     ws_stream: WebSocketStream<MaybeTlsStream<TcpStream>>,
 }
 
-enum ActiveSymbols {
-    Brief,
-    Full,
-}
-
 impl WebSocket {
     fn new(ws_stream: WebSocketStream<MaybeTlsStream<TcpStream>>) -> Self {
         Self { ws_stream }
@@ -27,6 +27,20 @@ impl WebSocket {
         let res = self.ws_stream.send(Message::Text(message.clone())).await;
         match res {
             Ok(_) => println!("Sent: {}", message),
+            Err(e) => println!("Error sending message: {}", e),
+        }
+    }
+
+    async fn subscribe<'a>(&'a mut self, message: String) {
+        let mut message = message.split("{").collect::<Vec<&str>>();
+        message[0] = "{\"subscribe\": 1, ";
+
+        let res = self
+            .ws_stream
+            .send(Message::Text(message.join("").to_string()))
+            .await;
+        match res {
+            Ok(_) => println!("Sent: {}", message.join("").to_string()),
             Err(e) => println!("Error sending message: {}", e),
         }
     }
@@ -49,10 +63,10 @@ impl WebSocket {
     }
 
     // public methods
-    pub async fn active_symbols<'a>(&'a mut self, active_symbols: ActiveSymbols) {
+    pub async fn active_symbols<'a>(&'a mut self, active_symbols: String) {
         let active_symbols = match active_symbols {
-            ActiveSymbols::Brief => "brief",
-            ActiveSymbols::Full => "full",
+            x if vec!["brief".to_string(), "full".to_string()].contains(&x) => x,
+            _ => panic!("Invalid active_symbols"),
         };
         let message = format!(
             r#"{{"active_symbols": "{}", "product_type": "basic"}}"#,
@@ -77,11 +91,16 @@ impl WebSocket {
         self.receive().await;
     }
 
-    pub async fn ping<'a>(&'a mut self, interval: u64) {
+    pub async fn ping<'a>(&'a mut self, sleep: u64) {
         let message = r#"{"ping": 1}"#.to_string();
+        match sleep {
+            0 => self.send(message.clone()).await,
+            _ => {
+                tokio::time::sleep(tokio::time::Duration::from_millis(sleep)).await;
+                self.subscribe(message.clone()).await
+            }
+        }
 
-        tokio::time::sleep(tokio::time::Duration::from_millis(interval)).await;
-        self.send(message.clone()).await;
         let res = self.receive().await;
         let res_json = self.string_to_json(res.unwrap().to_text().unwrap().to_string());
         self.print_json(res_json);
@@ -89,7 +108,7 @@ impl WebSocket {
 
     pub async fn ticks<'a>(&'a mut self, symbol: String) {
         let message = format!(r#"{{"ticks": "{}"}}"#, symbol).to_string();
-        self.send(message.clone()).await;
+        self.subscribe(message.clone()).await;
         let res = self.receive().await;
         let res_str = res.clone().unwrap().to_text().unwrap().to_string();
         let res_json = self.string_to_json(res_str);
@@ -113,7 +132,7 @@ async fn main() -> Result<()> {
 
     let (ws_stream, _response) = connect_async(url).await.expect("Failed to connect");
     println!("WebSocket handshake has been successfully completed");
-    let mut ws = WebSocket::new(ws_stream);
+    let ws = Arc::new(Mutex::new(WebSocket::new(ws_stream)));
 
     loop {
         print!("\n> ");
@@ -124,14 +143,15 @@ async fn main() -> Result<()> {
             let line = line.trim();
             match line.split_whitespace().next().unwrap() {
                 "exit" => {
-                    ws.close().await;
+                    ws.lock().unwrap().close().await;
                     return Ok(());
                 }
                 "active_symbols" => {
                     let active_symbols = line.split_whitespace().last().unwrap().to_string();
                     match active_symbols.as_str() {
-                        "brief" => ws.active_symbols(ActiveSymbols::Brief).await,
-                        "full" => ws.active_symbols(ActiveSymbols::Full).await,
+                        x if vec!["brief", "full"].contains(&x) => {
+                            ws.lock().unwrap().active_symbols(x.to_string()).await
+                        }
                         _ => {
                             println!("Invalid command");
                         }
@@ -139,20 +159,20 @@ async fn main() -> Result<()> {
                 }
                 "forget" => {
                     let id = line.split_whitespace().last().unwrap().to_string();
-                    ws.forget(id).await;
+                    ws.lock().unwrap().forget(id).await;
                 }
                 "ping" => {
-                    let interval = line.split_whitespace().last().unwrap().to_string();
-                    match interval.parse::<u64>() {
-                        Ok(interval_ms) => ws.ping(interval_ms).await,
-                        Err(_) => ws.ping(0).await,
+                    let sleep = line.split_whitespace().last().unwrap().to_string();
+                    match sleep.parse::<u64>() {
+                        Ok(sleep_ms) => ws.lock().unwrap().ping(sleep_ms).await,
+                        Err(_) => ws.lock().unwrap().ping(0).await,
                     }
                 }
                 "ticks" => {
                     let symbol = line.split_whitespace().last().unwrap().to_string();
-                    ws.ticks(symbol).await;
+                    ws.lock().unwrap().ticks(symbol).await;
                 }
-                "website_status" => ws.website_status().await,
+                "website_status" => ws.lock().unwrap().website_status().await,
                 _ => {
                     println!("Invalid command");
                 }
